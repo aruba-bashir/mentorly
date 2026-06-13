61f7ad2d5e1bd9c01fe3e1918e8d057e0ce45643
aruba <aruba9541@gmail.com>
2026-06-13 12:35:13 +0530

    resolved i guess

--- backend/src/middleware/importAuth.js
+++ backend/src/middleware/importAuth.js
@@ -1,4 +1,4 @@
-/*export const importAuth = (req, res, next) => {
+export const importAuth = (req, res, next) => {
   const secret = req.headers["x-import-secret"];
 
   if (secret !== process.env.IMPORT_SECRET) {
@@ -8,14 +8,5 @@
   }
 
   next();
-};*/
-
-export const importAuth = (req, res, next) => {
-  console.log("HEADER SECRET:", req.headers["x-import-secret"]);
-  console.log("ENV SECRET:", process.env.IMPORT_SECRET);
-
-  return res.status(200).json({
-    header: req.headers["x-import-secret"],
-    env: process.env.IMPORT_SECRET,
-  });
 };
+
